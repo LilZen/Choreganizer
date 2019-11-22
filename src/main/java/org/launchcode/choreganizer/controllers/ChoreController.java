82629a6779de93c37f075ed1f85af2ca7779c1ab
Melissa <bloomer.melissa18@gmail.com>
2019-11-22 13:04:51 -0500

    Created ChoreController, chore class, choreDao

--- src/main/java/org/launchcode/choreganizer/controllers/ChoreController.java
+++ src/main/java/org/launchcode/choreganizer/controllers/ChoreController.java
@@ -1,4 +1,25 @@
 package org.launchcode.choreganizer.controllers;
 
+import org.launchcode.choreganizer.models.data.ChoreDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.persistence.Id;
+
+@Controller
 public class ChoreController {
+
+    @Autowired
+    ChoreDao choreDao;
+
+    @RequestMapping(value="home")
+    public String findChore(Model model){
+
+        model.addAttribute("chores", choreDao.findAll());
+        model.addAttribute("title", "Chores");
+
+        return "home";
+    }
 }
