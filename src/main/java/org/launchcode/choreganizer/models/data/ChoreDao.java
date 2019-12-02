6f9c56c76fd15ceeb56b09d24480d8b12d974413
Melissa <bloomer.melissa18@gmail.com>
2019-12-02 16:14:59 -0500

    Updated Dao with @Repository and @Transactional

--- src/main/java/org/launchcode/choreganizer/models/data/ChoreDao.java
+++ src/main/java/org/launchcode/choreganizer/models/data/ChoreDao.java
@@ -2,6 +2,11 @@ package org.launchcode.choreganizer.models.data;
 
 import org.launchcode.choreganizer.models.Chore;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface ChoreDao extends CrudRepository<Chore, Integer> {
 }
--- src/main/java/org/launchcode/choreganizer/models/data/CleanerDao.java
+++ src/main/java/org/launchcode/choreganizer/models/data/CleanerDao.java
@@ -2,6 +2,11 @@ package org.launchcode.choreganizer.models.data;
 
 import org.launchcode.choreganizer.models.Cleaner;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface CleanerDao extends CrudRepository<Cleaner, Integer> {
 }
