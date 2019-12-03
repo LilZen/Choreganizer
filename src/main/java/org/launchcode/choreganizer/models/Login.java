628c96d2bad3e685a01f6c626d809a594eb0ca55
Melissa <bloomer.melissa18@gmail.com>
2019-12-03 10:08:09 -0500

    Updated login, and loginController to create database

--- src/main/java/org/launchcode/choreganizer/models/Login.java
+++ src/main/java/org/launchcode/choreganizer/models/Login.java
@@ -1,10 +1,18 @@
 package org.launchcode.choreganizer.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class Login {
+
+    @Id
+    @GeneratedValue
+    private int Id;
 
     @NotNull
     @Size(min= 5, max= 15)
